2eff3a8280cf2599786e008f5812fe8f0f8c5f7d
Chitsanupong <first30952@gmail.com>
2021-09-19 10:47:24 +0700

    feat: animate, initial and transition

--- framer-motion/src/pages/Home.js
+++ framer-motion/src/pages/Home.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import classes from './Home.module.css';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div>
-      <h2>Home Page</h2>
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.2, duration: 1 }}
+      className={classes.container}
+    >
+      <h2>Framer Motion animation</h2>
+      <Link to="/button">
+        <motion.button className={classes.button}>
+          Go to Button Showcase
+        </motion.button>
+      </Link>
+      <motion.p
+        initial={{ x: '-50vw' }}
+        animate={{ x: 0 }}
+        transition={{
+          delay: 0.2,
+          duration: 1,
+          type: 'spring',
+        }}
+      >
+        transition
+      </motion.p>
+    </motion.div>
   );
 };
 
