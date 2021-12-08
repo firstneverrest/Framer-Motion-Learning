1298ec7fbeacbd916a5f87e5c9d2c20f72cd2d55
Chitsanupong <first30952@gmail.com>
2021-12-08 10:38:18 +0700

    feat: add variant & orchestration

--- framer-motion/src/pages/Home.js
+++ framer-motion/src/pages/Home.js
@@ -4,16 +4,44 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
 const Home = () => {
+  const containerVariants = {
+    visible: {
+      opacity: 1,
+      x: 100,
+      transition: {
+        duration: 1,
+        when: 'beforeChildren',
+        staggerChildren: 0.5,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: 0,
+    },
+  };
+
+  const childVariants = {
+    visible: {
+      opacity: 1,
+    },
+    hidden: {
+      opacity: 0,
+    },
+  };
+
   return (
     <motion.div
-      initial={{ x: 0, opacity: 0 }}
-      animate={{ x: 100, opacity: 1 }}
-      transition={{ delay: 0.2, duration: 1 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
       className={classes.container}
     >
       <h2>Framer Motion animation</h2>
+      <motion.p variants={childVariants}>Child 1</motion.p>
+      <motion.p variants={childVariants}>Child 2</motion.p>
       <Link to="/button">
         <motion.button
+          variants={childVariants}
           whileHover={{ backgroundColor: '#000' }}
           transition={{ duration: 0.5 }}
           className={classes.button}
