5a427c1fd60562f0e900093a69375adaf4f53ed2
Chitsanupong <first30952@gmail.com>
2021-12-08 09:32:33 +0700

    feat: add hover animation

--- framer-motion/src/pages/Home.js
+++ framer-motion/src/pages/Home.js
@@ -6,28 +6,21 @@ import { motion } from 'framer-motion';
 const Home = () => {
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      initial={{ x: 0, opacity: 0 }}
+      animate={{ x: 100, opacity: 1 }}
       transition={{ delay: 0.2, duration: 1 }}
       className={classes.container}
     >
       <h2>Framer Motion animation</h2>
       <Link to="/button">
-        <motion.button className={classes.button}>
+        <motion.button
+          whileHover={{ backgroundColor: '#000' }}
+          transition={{ duration: 0.5 }}
+          className={classes.button}
+        >
           Go to Button Showcase
         </motion.button>
       </Link>
-      <motion.p
-        initial={{ x: '-50vw' }}
-        animate={{ x: 0 }}
-        transition={{
-          delay: 0.2,
-          duration: 1,
-          type: 'spring',
-        }}
-      >
-        transition
-      </motion.p>
     </motion.div>
   );
 };
